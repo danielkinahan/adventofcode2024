be47dff453beed65093cc3b74721fc211a0c97ad
Daniel Kinahan <github@danielkinahan.ca>
2024-12-01 18:49:15 -0500

    Completed day 1 first star

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    day_one();
+}
+
+fn day_one() {
+    // Get length of file
+    let mut total = 0;
+    // Make an array of ints for each list
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list: Vec<i32> = Vec::new();
+    // Read file of historically significant location IDs
+    for line in read_to_string("./input/1").unwrap().lines() {
+        //Split by the space to get two list elements
+        let item_pair: Vec<&str> = line.split_whitespace().collect();
+        left_list.push(item_pair[0].parse::<i32>().unwrap());
+        right_list.push(item_pair[1].parse::<i32>().unwrap());
+    }
+    left_list.sort();
+    right_list.sort();
+
+    for index in 0..left_list.len() {
+        total += right_list[index].abs_diff(left_list[index]);
+    }
+
+    println!("{}", total);
 }
