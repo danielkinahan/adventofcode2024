f66e1f6f39af5bc5f2aa234b5214bd5be2120ef6
danielkinahan <github@danielkinahan.ca>
2024-12-02 15:50:57 -0500

    complete day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,72 @@
+use std::env;
 use std::fs::read_to_string;
 
 fn main() {
-    day_one();
-    day_two();
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    day_one(path);
+    day_two(path);
 }
 
-fn day_two() {
+fn day_two(path: &str) {
     let mut total: u32 = 0;
-    for line in read_to_string("./input/2").unwrap().lines() {
+    for line in read_to_string(path.to_owned() + "2").unwrap().lines() {
         // Determine if consistently increasing or decreasing
         let report: Vec<u32> = line
             .split_whitespace()
             .map(|s| s.parse::<u32>().unwrap())
             .collect();
-        let mut last_item: u32 = 0;
-        let increasing: bool = report[0] < report[1];
-        let mut safe: bool = true;
-        for item in report {
-            if last_item == 0 {
-                last_item = item;
-                continue;
-            }
-            let difference: i32 = item as i32 - last_item as i32;
-            // if absolute value of difference is less than one or greater than 3, break and mark unsafe
-            if difference.abs() < 1 || difference.abs() > 3 {
-                safe = false;
-                break;
-            }
-            // if difference is negative (decreasing) and increasing is true, break and mark unsafe
-            if difference < 0 && increasing == true {
-                safe = false;
-                break;
-            }
-            // if, conversely, difference is positive but increasing is false, break
-            if difference > 0 && increasing == false {
-                safe = false;
-                break;
-            }
-            // else carry on
-            last_item = item;
-        }
-        if safe {
+        if is_report_safe(&report, false) {
+            println!("{:?} is safe", report);
             total += 1;
         }
     }
     println!("{}", total)
 }
 
-fn day_one() {
+fn is_report_safe(report: &Vec<u32>, problem_dampener_used: bool) -> bool {
+    let mut last_item: u32 = 0;
+    let increasing: bool = report[0] < report[1];
+    let mut safe: bool = true;
+    for index in 0..report.len() {
+        let item = report[index];
+        if last_item == 0 {
+            last_item = item;
+            continue;
+        }
+        let difference: i32 = item as i32 - last_item as i32;
+        // if absolute value of difference is less than one or greater than 3, break and mark unsafe
+        // if difference is negative (decreasing) and increasing is true, break and mark unsafe
+        // if, conversely, difference is positive but increasing is false, break
+        if difference.abs() < 1
+            || difference.abs() > 3
+            || (difference < 0 && increasing)
+            || (difference > 0 && !increasing)
+        {
+            if problem_dampener_used {
+                safe = false;
+                break;
+            } else {
+                let mut found_subreport = false;
+                for j in 0..report.len() {
+                    let mut sub_report = report.clone();
+                    sub_report.remove(j);
+                    if is_report_safe(&sub_report, true) {
+                        println!("Sub-report found {:?}", sub_report);
+                        found_subreport = true;
+                        break;
+                    }
+                }
+                safe = found_subreport
+            }
+        }
+        // else carry on
+        last_item = item;
+    }
+    safe
+}
+
+fn day_one(path: &str) {
     // Get length of file
     let mut first_total = 0;
     let mut second_total = 0;
@@ -55,7 +74,7 @@ fn day_one() {
     let mut left_list: Vec<i32> = Vec::new();
     let mut right_list: Vec<i32> = Vec::new();
     // Read file of historically significant location IDs
-    for line in read_to_string("./input/1").unwrap().lines() {
+    for line in read_to_string(path.to_owned() + "1").unwrap().lines() {
         //Split by the space to get two list elements
         let item_pair: Vec<&str> = line.split_whitespace().collect();
         left_list.push(item_pair[0].parse::<i32>().unwrap());
